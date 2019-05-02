5f83780666d5efa6fc28262985540c72aa569c07
Ibrahim Umar <ibrahim.umar@imr.no>
2019-05-02 14:17:59 +0200

    Remove reference to g2c

--- build.sh
+++ build.sh
@@ -7,12 +7,11 @@ tar -zxvf taucs.tgz
 patch -p1 < taucs-2.2.patch
 
 sed -i 's|-lf77blas -lcblas -latlas|-lblas|' config/linux.mk
-
+sed -i 's|-lg2c| |' config/linux.mk
 
 # For linux
 mkdir -p external/lib/linux/
 
-ln -s /usr/lib64/libg2c*.a external/lib/linux/libg2c.a
 ln -s /usr/lib64/libblas.so external/lib/linux/libblas.so
 ln -s /usr/lib64/liblapack.so external/lib/linux/liblapack.so
 
