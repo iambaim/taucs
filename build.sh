51d571dccdf06763e79d29b653a2b72ec5223d5d
Ibrahim Umar <ibrahim.umar@uit.no>
2019-05-05 08:47:25 +0200

    Update OSX and Linux build script

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# For linux
-mkdir -p external/lib/linux/
-
-ln -s /usr/lib64/libblas.so external/lib/linux/libblas.so
-ln -s /usr/lib64/liblapack.so external/lib/linux/liblapack.so
-
 # Configure and make
 ./configure
 
-make OPT_FLAGS="$RPM_OPT_FLAGS -fPIC" LIBBLAS=-lblas LIBLAPACK=-llapack
+make OPT_FLAGS="-fPIC" LDFLAGS="-fPIC"
 
