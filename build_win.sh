84e3f130f3f212e988640aa64cc21c6754c774d2
Ibrahim Umar <ibrahim.umar@imr.no>
2019-05-06 16:41:17 +0200

    Metis building on windows

--- build_win.sh
+++ build_win.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Build METIS
+curl -o metis.tgz http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz
+tar -zxvf metis.tgz
+cd metis-4.0.3
+make
+export LMETIS_LOC=-L`pwd`
+cd ..
+
 # Start build
 
 make -f configurator/makefile.unix
@@ -8,4 +16,4 @@ export OSTYPE=linux
 
 configurator/configurator
 
-make LDFLAGS="-fPIC -D__USE_MINGW_ANSI_STDIO=1" OPT_FLAGS="-fPIC -D__USE_MINGW_ANSI_STDIO=1"
+make LDFLAGS="-fPIC -D__USE_MINGW_ANSI_STDIO=1 $LMETIS_LOC" OPT_FLAGS="-fPIC -D__USE_MINGW_ANSI_STDIO=1"
